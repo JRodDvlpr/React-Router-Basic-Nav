653fae5d1bf85fb4c4d24d63744fb2fc63edddbf
JRodDvlpr <Mialienproductions@gmail.com>
2019-12-04 12:37:18 -0500

    wrapped router to App

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <Router> 
+        <App /> 
+    </Router>, 
+    rootElement
+);
